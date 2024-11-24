c2077f26f61aef986f2779f433171ee7caec0288
Akib Hossain <76858240+akib16301028@users.noreply.github.com>
2024-11-24 09:18:43 +0600

    Update AlarmReportGenerator.py

--- AlarmReportGenerator.py
+++ AlarmReportGenerator.py
@@ -214,7 +214,7 @@ def style_dataframe(df, duration_cols, is_dark_mode):
     
     # Apply background color to cells with 0 or empty values
     def highlight_zero(val):
-        if val == 0 or val == "":
+        if val != 0 :
             return f'background-color: {cell_bg_color}; color: {font_color}'
         return ''
     
