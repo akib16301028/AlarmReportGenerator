746130e30f99100100ca4e5b1814d8105155b812
Akib Hossain <76858240+akib16301028@users.noreply.github.com>
2025-01-06 21:55:56 +0600

    Update AlarmReportGenerator.py

--- AlarmReportGenerator.py
+++ AlarmReportGenerator.py
@@ -173,7 +173,8 @@ def create_offline_pivot(df):
     return pivot, total_offline_count
 
 # Function to calculate time offline smartly (minutes, hours, or days)
-def calculate_duration(df, current_time):
+def calculate_duration(df):
+    current_time = datetime.now()
     df['Last Online Time'] = pd.to_datetime(df['Last Online Time'], format='%Y-%m-%d %H:%M:%S')
     df['Hours Offline'] = (current_time - df['Last Online Time']).dt.total_seconds() / 3600
 
